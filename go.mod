b021edb36bf17a52ddb54d5cfd776f55392b461d
Ingrid Caceres <ingridcaceres36@gmail.com>
2023-11-07 17:07:53 -0500

    Adicion de las funciones Actualizar y Consultar Productos

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/programaIng/software_inventarios
+
+go 1.20
--- inventarios.go
+++ inventarios.go
@@ -3,51 +3,54 @@ package main
 import "fmt"
 
 var (
-	sliceProducto []productos
-	mapProducto   = make(map[string]productos)
+	sliceProducto []producto
+	mapProducto   = make(map[string]producto)
 )
 
-func main() {
-
-	//Todo este codigo se puede meter en una función tambien para que en el main no haya tanto
-	mapProducto["Trululu"] = productos{
+func inventario() {
+	mapProducto["Trululu"] = producto{
 		nombre:   "Trululu",
 		precio:   2500,
 		cantidad: 10,
 	}
 
-	mapProducto["Pirulito"] = productos{
+	mapProducto["Pirulito"] = producto{
 		nombre:   "Pirulito",
 		precio:   5000,
 		cantidad: 100,
 	}
 
-	mapProducto["Festival"] = productos{
+	mapProducto["Festival"] = producto{
 		nombre:   "Festival",
 		precio:   7800,
 		cantidad: 120,
 	}
 
-	mapProducto["Cofee"] = productos{
+	mapProducto["Cofee"] = producto{
 		nombre:   "Cofee",
 		precio:   2400,
 		cantidad: 80,
 	}
 
-	mapProducto["Trocipollo"] = productos{
+	mapProducto["Trocipollo"] = producto{
 		nombre:   "Trocipollo",
 		precio:   8000,
 		cantidad: 95,
 	}
 
-	sliceProducto = []productos{
+	sliceProducto = []producto{
 		mapProducto["Trululu"],
 		mapProducto["Pirulito"],
 		mapProducto["Festival"],
 		mapProducto["Cofee"],
 		mapProducto["Trocipollo"],
 	}
-	//hasta aqui
+
+}
+
+func main() {
+
+	inventario()
 
 	actividadDiaria()
 
@@ -56,7 +59,7 @@ func main() {
 // producto esta estructura detalla cada producto
 //Como dices struct es un objeto, objeto producto que tiene caracteristicas que podemos usar en las funciones
 //estas cayendo en el error de comparar todo un objeto con un nombre es como compar a un Carro completo con una palabra y decir que son lo mismo
-type productos struct {
+type producto struct {
 	nombre   string
 	precio   int16
 	cantidad int16
@@ -65,7 +68,7 @@ type productos struct {
 //Esta función esta muy bien
 func actividadDiaria() {
 	var nombreActividadRealizar string
-	fmt.Println(" Ingresa la actividad que deseas realizar: Crear, Eliminar, Adicionar, Consultar ")
+	fmt.Println(" Ingresa la actividad que deseas realizar: Crear, Eliminar, Actualizar, Consultar ")
 	fmt.Scanln(&nombreActividadRealizar)
 	fmt.Println(" Ingresaste: ", nombreActividadRealizar)
 
@@ -75,21 +78,15 @@ func actividadDiaria() {
 		ingresoProductos(productoNuevo)
 	case "Eliminar":
 		eliminacionProducto()
-	case "Adicionar":
-		productoAdicionado := ingresoActualizacionCantidad()
-		actualizarProductos(productoAdicionado)
+	case "Actualizar":
+		productoActualizado := ingresoProductoActualizar()
+		actualizarProductos(productoActualizado)
 	case "Consultar":
-		//Esto se puede meter en la función consultar productos, no crees?
-		var seleccionProducto string
-		fmt.Println("Ingrese el producto a consultar:")
-		fmt.Scanln(&seleccionProducto)
-		consultaProductos(seleccionProducto)
-	default:
-		fmt.Println("El valor ingresado no es válido:")
+		consultaProductos()
 	}
 }
 
-func ingresoProductosNuevos() productos {
+func ingresoProductosNuevos() producto {
 	var nombreProductoNuevo string
 	fmt.Println(" Ingresa el producto nuevo: ")
 	fmt.Scanln(&nombreProductoNuevo)
@@ -106,7 +103,7 @@ func ingresoProductosNuevos() productos {
 	fmt.Println(" La cantidad de Producto Nuevo es: ", cantidadProductoNuevo)
 
 	//repasa esto por fa la declaración de struct y como se usan
-	var productoNuevo productos
+	var productoNuevo producto
 	productoNuevo.nombre = nombreProductoNuevo
 	productoNuevo.precio = precioProductoNuevo
 	productoNuevo.cantidad = cantidadProductoNuevo
@@ -120,7 +117,7 @@ func ingresoProductosNuevos() productos {
 }
 
 // ingresoProductos esta función agrega productos al inventario
-func ingresoProductos(productoNuevo productos) {
+func ingresoProductos(productoNuevo producto) {
 
 	sliceProducto = append(sliceProducto, productoNuevo)
 	mapProducto[productoNuevo.nombre] = productoNuevo
@@ -147,48 +144,53 @@ func eliminacionProducto() {
 }
 
 // adicionarProductos esta función permite adicionar la cantidad de productos disponibles en el inventario
-func ingresoActualizacionCantidad() productos {
+func ingresoProductoActualizar() producto {
 	var nombreProductoActualizar string
-	fmt.Println(" Ingresa el nombre del producto a Adicionar: ")
+	fmt.Println(" Ingresa el nombre del producto a Actualizar: ")
 	fmt.Scanln(&nombreProductoActualizar)
 	fmt.Println(" Ingresaste: ", nombreProductoActualizar)
 
 	var cantidadProductoActualizar int16
-	fmt.Println(" Ingresa la Cantidad de producto a Adicionar: ")
+	fmt.Println(" Ingresa la Cantidad de producto a Actualizar: ")
 	fmt.Scanln(&cantidadProductoActualizar)
-	fmt.Println(" La cantidad de Producto Adicionado es: ", cantidadProductoActualizar)
+	fmt.Println(" La cantidad de Producto Actualizar es: ", cantidadProductoActualizar)
 
-	var productoActualizado productos
+	var productoActualizado producto
 	productoActualizado.nombre = nombreProductoActualizar
 	productoActualizado.cantidad = cantidadProductoActualizar
+	productoActualizado.precio = mapProducto[nombreProductoActualizar].precio
 
 	return productoActualizado
 
 }
 
-// actualizarProductos esta función agrega productos al inventario
-func actualizarProductos(productoActualizado) {
+// actualizarProductos esta función actualiza la cantidad de producto disponible
+func actualizarProductos(productoActualizado producto) {
 
 	for posicion, productoActualizar := range sliceProducto {
-		if nombreProductoActualizar == productoActualizado.nombre{ 
-			sliceProducto = map [productoActualizado.nombre]  //ya el map esta declarado en la linea 7 ahora debes trabajar con el nombre que seria mapProducto
-			// recuerda que para obtener el valor de un map seria mapProductos[productoActualizado.nombre] porque nuestro map es asi map[string]productos recuerda lo del diccionario
-			mapProducto = productoActualizar.cantidad 
-			
-		} 
+		if productoActualizar.nombre == productoActualizado.nombre {
+			mapProducto[productoActualizado.nombre] = productoActualizado
+			sliceProducto[posicion] = productoActualizado
+			fmt.Printf("El producto: %s, Usted actualizó: %d", productoActualizado.nombre, productoActualizado.cantidad)
+			fmt.Printf("Productos Slice: %v, Productos Map: %v", sliceProducto, mapProducto)
+		}
+
 	}
-	sliceProducto = append(sliceProducto, productoActualizado)//Para lo que quieres hacer, esto es incorrecto, tienes que hacer un for
-	mapProducto[productoActualizado.nombre] = productoActualizado
-	fmt.Printf("Al producto: %s, Usted adicionó: %d", productoActualizado.nombre, productoActualizado.cantidad)
-	//fmt.Println(sliceProducto, mapProducto)
+
 }
 
-// consultaProductos esta funcion muestra los productos disponibles
-func consultaProductos(seleccionProducto string) {
+// consultaProductos esta funcion muestra los datos de los productos disponibles
 
-	for _, producto := range sliceProducto {
-		if producto.nombre == seleccionProducto {// si ves que aqui comparas producto.nombre que es un string con seleccionProducto que es un string solo que  producto.nombre hace parte de un struct
-			fmt.Println(producto.nombre, producto.cantidad, producto.precio)
+func consultaProductos() {
+
+	var nombreProductoConsultar string
+	fmt.Println("Ingrese el nombre del producto a consultar:")
+	fmt.Scanln(&nombreProductoConsultar)
+	fmt.Println(" El producto a consultar es: ", nombreProductoConsultar)
+
+	for _, productoActualizar := range sliceProducto {
+		if productoActualizar.nombre == nombreProductoConsultar {
+			fmt.Println(mapProducto[nombreProductoConsultar])
 			return
 		}
 
@@ -199,7 +201,7 @@ func consultaProductos(seleccionProducto string) {
 
 //En general bien, importante repasar los structs y como funcionan, creo que es una oportunidad de mejora
 // Repasar tambien las funciones, tenemos algunas dudas en los parametros de entrada y de salida, ademas, de no tener claro como se utilizan dentro de la func
-// recuerda que: 
+// recuerda que:
 //escribir el flujo primero y luego hacerlo, intentas crearlo en tu mente y al principio es una tareea que nos cuesta, escribir el paso a paso nos va a ayudara entender el flujo
 //"La resiliencia no es la ausencia de dificultades, sino la capacidad de enfrentarlas, superarlas y crecer con cada desafío."
 //"El aprendizaje es un tesoro que seguirá contigo dondequiera que vayas." - Oprah Winfrey
